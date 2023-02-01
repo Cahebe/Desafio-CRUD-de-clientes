596aea2d0d34a7d2533ea0c95b61e4131756ba56
Cahebe <cahebe@gmail.com>
2023-01-31 22:58:25 -0300

    Seed DB

---
+++ src/main/java/com/devsuperior/clients/entities/Client.java
@@ -0,0 +1,22 @@
+package com.devsuperior.clients.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true)
+    private String cpf;
+    private Double income;
+    private LocalDate birthDate;
+    private Integer children;
+
+    
+}
