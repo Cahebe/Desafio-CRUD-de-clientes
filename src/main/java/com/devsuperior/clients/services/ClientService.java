a762254f8f7d5cbb24622afd38a145b6b3796a8d
Cahebe <cahebe@gmail.com>
2023-02-02 20:23:58 -0300

    ResponseEntity

---
+++ src/main/java/com/devsuperior/clients/services/ClientService.java
@@ -0,0 +1,45 @@
+package com.devsuperior.clients.services;
+
+import com.devsuperior.clients.dto.ClientDTO;
+import com.devsuperior.clients.entities.Client;
+import com.devsuperior.clients.repositories.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@Service
+public class ClientService {
+    @Autowired
+    private ClientRepository repository;
+
+
+    @Transactional(readOnly = true)
+    public ClientDTO findByID(Long id) {
+        Client client = repository.findById(id).get();
+        return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
+
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+}
